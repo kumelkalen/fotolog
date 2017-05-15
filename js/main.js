bd4739fe7d0c4a1dea1c822cb1af3c5994836a4b
kumelkalen <free.your.mind.fc@gmail.com>
2017-05-15 12:25:40 -0400

    Dejamos el nombre y el contenido como un nodo de texto

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ function agregaPosteo() {
 var containerPosteos = document.getElementById("contenedor-posteos"); nombreFF = document.getElementById("nombre").value; contenidoFF = document.getElementById("cajaposteos").value; nuevoPost = document.createElement("div");
 var contenedorNombre = document.createElement("strong") ; contenedorPost = document.createElement("p"); separacion = document.createElement("hr");
 var parrafCorazon = document.createElement("p"); i = document.createElement("i");
+
 parrafCorazon.appendChild(i);
+parrafCorazon.setAtribute("clas","corazon");
+i.setAtribute("class","fa fa-heart");
+i.setAtribute("arial-hidden","true");
 
-
+var nodoNombre = document.createTextNode(nombreFF +" escribió:"); nodoPosteo = document.createTextNode(contenidoFF);
 }
