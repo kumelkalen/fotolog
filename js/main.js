b5c92030b28f16972ef8db82f0289016cf4ecba4
kumelkalen <free.your.mind.fc@gmail.com>
2017-05-15 12:15:17 -0400

    creamos una variable donde guardaremos el icono del corazón y le asignamos un lugar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 function agregaPosteo() {
-var containerPosteos = document.getElementById("contenedor-posteos");
-var nombreFF = document.getElementById("nombre").value;
-var contenidoFF = document.getElementById("cajaposteos");
+var containerPosteos = document.getElementById("contenedor-posteos"); nombreFF = document.getElementById("nombre").value; contenidoFF = document.getElementById("cajaposteos").value; nuevoPost = document.createElement("div");
+var contenedorNombre = document.createElement("strong") ; contenedorPost = document.createElement("p"); separacion = document.createElement("hr");
+var parrafCorazon = document.createElement("p"); i = document.createElement("i");
+parrafCorazon.appendChild(i);
+
+
 }
